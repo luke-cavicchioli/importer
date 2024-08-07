cf6c1b8721b6bba90b4e7725a133074d50eaa675
luke_cavicchioli <lucavicchioli8@gmail.com>
2024-08-07 12:22:29 +0200

    Added basic input path selection functionality

--- importer/main.py
+++ importer/main.py
@@ -1,19 +1,22 @@
 """Main function of the program."""
 
+import glob
 import ipaddress
 import logging
 import os
 import pathlib
 import sys
 import warnings
-from datetime import date
-from typing import Optional
+from datetime import date, datetime
+from typing import List, Optional
 
 import click
+import questionary
 import sh
 from rich.console import Console
 from rich.logging import RichHandler
 from rich.style import Style
+from thefuzz import fuzz
 
 termw = 80
 
@@ -72,7 +75,7 @@ def handle_today_arg(kwargs):
 
     newargs = kwargs.copy()
     newargs.update({
-        "datepath": date.today(),
+        "datepath": date.today().strftime("%Y-%m-%d"),
         "inpath": None
     })
     return newargs
@@ -130,7 +133,7 @@ def ping_server(flag: bool, ip: str) -> bool:
         times = time_line[3].split("/")
         tmin = times[0]
         tmax = times[1]
-        unit = time_line[4]
+        unit = time_line[4][:-1]
         time_str = f"Ping ({unit}): min {tmin}, max {tmax}"
 
         logger.info(
@@ -141,8 +144,20 @@ def ping_server(flag: bool, ip: str) -> bool:
     return True
 
 
-def mount_repo(mountpoint: Optional[str]) -> bool:
+def mount_remote(mountpoint: Optional[str]) -> bool:
+    """Mount remote directory to specified mountpoint."""
     logger.debug(f"{mountpoint = }")
+    ret = sh.mountpoint(
+        mountpoint,
+        "-q",
+        _return_cmd=True,
+        _ok_code=[0, 32],
+        _err_to_out=True
+    )
+    if ret.exit_code == 0:
+        logger.debug(f"{mountpoint} is already mounted.")
+        return True
+
     ret = sh.mount(
         mountpoint,
         _return_cmd=True,
@@ -156,6 +171,113 @@ def mount_repo(mountpoint: Optional[str]) -> bool:
     logger.error(ret)
 
     return False
+
+
+def unmount_remote(mountpoint: Optional[str]):
+    """Try to unmount the remote directory."""
+    if mountpoint is not None:
+        ret = sh.umount(
+            mountpoint,
+            _return_cmd=True,
+            _ok_code=[0, 1, 2, 4, 8, 16, 32, 64]
+        )
+        if ret.exit_code != 0:
+            warnings.warn("Could not unmount the remote directory.")
+        logger.debug(f"Unmounting {mountpoint}: {ret.exit_code = }")
+    else:
+        logger.debug("Skipping unmounting")
+
+
+def get_inpath(root: Optional[str]) -> pathlib.Path:
+    """Interactively get the input path."""
+    root_path = None
+    if root is None:
+        root_path = pathlib.Path("./").resolve()
+    else:
+        root_path = pathlib.Path(root).resolve()
+    logger.debug(f"{root_path = }")
+
+    ans = questionary.path(
+        message="Choose input path: ",
+        default=str(root_path),
+        only_directories=True,
+    ).ask()
+    logger.debug(f"{ans = }")
+
+    if ans is None:
+        return root_path
+    else:
+        return pathlib.Path(ans)
+
+
+def handle_datedir(kwargs):
+    datepath = kwargs["datepath"]
+    inpath = kwargs["inpath"]
+    mountpoint = kwargs["mountpoint"]
+
+    if mountpoint is None:
+        mountpoint = os.getcwd()
+    mountpoint = pathlib.Path(mountpoint)
+
+    if datepath is not None:
+        if inpath is not None:
+            warnings.warn("date will override inpath")
+        datepath = datetime.strptime(datepath, "%Y-%m-%d")
+        with cns.status("Searching for matching directories"):
+            found = find_datedir(datepath, mountpoint)
+        inpath = select_datedir(found)
+
+    newargs = kwargs.copy()
+    newargs.update({"inpath": inpath})
+
+    return newargs
+
+
+def filterscore_subdirs(dirpath, dirnames, target):
+    """Sort and filter the subdirectories according to score."""
+    dirnames = sorted(
+        dirnames,
+        key=lambda x: fuzz.partial_ratio(str(x), target),
+        reverse=True
+    )
+    subdirs = [pathlib.Path(dirpath).joinpath(d) for d in dirnames]
+    scores = [(s, fuzz.partial_ratio(str(s), target)) for s in subdirs]
+    curr_score = fuzz.partial_ratio(str(dirpath), target)
+    return [s[0] for s in scores if s[1] > curr_score]
+
+
+def find_datedir(date: datetime, root: pathlib.Path, n=10) -> List[pathlib.Path]:
+    """Find best matching directories in the root path."""
+    target = date.strftime("%Y %m %d")
+    logger.debug(f"{target = }")
+    found = []
+
+    for dirpath, dirnames, _ in os.walk(root):
+
+        dirnames[:] = filterscore_subdirs(dirpath, dirnames, target)
+
+        if len(dirnames) == 0:
+            found.append(dirpath)
+            logger.debug(f"Appending to found: {dirpath}")
+
+        if len(found) >= n:
+            break
+
+    found = [pathlib.Path(p) for p in found]
+
+    return found
+
+
+def select_datedir(found: List[pathlib.Path]) -> pathlib.Path:
+    foundstr = [str(x) for x in found]
+    ans = questionary.select(
+        "Best matches:",
+        instruction="Enter to select the desired directory, C-c to cancel",
+        choices=foundstr,
+        show_selected=True
+    ).ask()
+    logger.debug(f"{ans = }")
+    return ans
 
 
 @click.group(
@@ -174,7 +296,7 @@ def mount_repo(mountpoint: Optional[str]) -> bool:
     "--date",
     "datepath",
     default=None,
-    type=date,
+    type=str,
     help="Import from the server a directory with this date as a name.",
 )
 @click.option(
@@ -198,7 +320,7 @@ def mount_repo(mountpoint: Optional[str]) -> bool:
     "-r",
     "--repopath",
     "repopath",
-    default=None,
+    default="./",
     type=pathlib.Path,
     help="Path of data repository",
     envvar="IMPORTER_REPOPATH",
@@ -279,8 +401,18 @@ def main(ctx, **kwargs):
             return 1
 
         with cns.status("Mounting remote repository"):
-            mount_repo(mountpoint)
+            mount_remote(mountpoint)
     else:
         logger.info("No mountpoint specified.")
+
+    kwargs = handle_datedir(kwargs)
+
+    inpath = kwargs["inpath"]
+    if inpath is not None:
+        pass
+    else:
+        cns.print("[bold yellow]No input path.")
+
+    unmount_remote(mountpoint)
 
     return 0
